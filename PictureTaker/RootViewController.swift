dba5d3350f11af25ddc5cf6f1bd2f42c547d422e
Joshua Dodd <joshuadodd@gmail.com>
2017-10-27 10:41:33 -0400

    Initial Commit

---
+++ PictureTaker/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  PictureTaker
+//
+//  Created by joshua dodd on 10/27/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
